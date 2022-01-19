c7fa3e14b5306e9c986e504b65e0d1204478befd
aliya25 <3103120172@student.smktelkom-pwt.sch.id>
2022-01-19 08:55:56 +0700

    commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Aliya | About</title>
-</head>
-<body>
+@extends('layouts.main')
+@section('container')
     <h1> Halaman About </h1>
-    <h3> <?php echo $nama; ?> </h3>
-    <h3> <?php echo $email; ?> </h3>
-    <img src="image/njun.jpg" alt="renjun" width="200px">
-</body>
-</html>
+    <h3> {{ $nama }} </h3>
+    <h3> {{ $email }} </h3>
+    <img src="image/{{ $gambar }}" alt="{{ $nama }}" width="200px">
+@endsection
